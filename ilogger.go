f8ad26891736a5112073b530d72585bf9534a8aa
louhao <lou.hao@bestv.com.cn>
2019-12-26 08:45:39 +0800

    修改接口文件

--- ilogger.go
+++ ilogger.go
@@ -1,17 +1,17 @@
 package logger
 
-type FormatFunc func(logType LogType, i interface{}) (string, []interface{}, bool)
+type FormatFunc func( LogType, interface{}) (string, []interface{}, bool)
 
 // 定义日志接口
 type ILogger interface {
 	func Init()
-	func SetLogLevel(ltyp LogType)
+	func SetLogLevel(LogType)
 	func GetLogLevel()
-	func SetLoggerFormat(func FormatFunc)
-	func Debug(i interface{})
-	func Info(i interface{})
-	func Notice(i interface{})
-	func Warn(i interface{})
-	func Error(i interface{})
-	func Fatal(i interface{})
+	func SetLoggerFormat(FormatFunc)
+	func Debug(interface{})
+	func Info(interface{})
+	func Notice(interface{})
+	func Warn(interface{})
+	func Error(interface{})
+	func Fatal(interface{})
 }
